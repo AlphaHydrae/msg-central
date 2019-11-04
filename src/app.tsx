09fc2592b58046842c7ea44edd6c3081b9e44f58
Simon Oulevay <simon.oulevay@gmail.com>
2019-11-04 21:13:21 +0100

    Add bootstrap

--- src/app.tsx
+++ src/app.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Navbar } from 'react-bootstrap';
 
-import './app.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
-import logo from './logo.svg';
-
-export const App: React.FC = () => {
+export function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Navbar bg='light' expand='lg'>
+        <Navbar.Brand href='#home'>ASAP</Navbar.Brand>
+      </Navbar>
+    </Fragment>
   );
 };
